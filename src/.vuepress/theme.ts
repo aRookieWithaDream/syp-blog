3a26c53aa0e6fbbe248d2205379fd17bf3ce0d2c
syp <syp13903995153@foxmail.com>
2025-03-07 10:02:29 +0800

    fix: 删除开发日志

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,6 +1,6 @@
 import { hopeTheme } from "vuepress-theme-hope";
-import navbar from "./navbar.js";
-import sidebar from "./sidebar.js";
+// import navbar from "./navbar.js";
+// import sidebar from "./sidebar.js";
 
 export default hopeTheme({
   hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
